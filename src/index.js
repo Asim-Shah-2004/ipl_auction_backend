6196dfa3b3dce84ef68737ae23dff676d0911ba1
Abhinav Pandey <121815498+LanceHoenhymm@users.noreply.github.com>
2023-12-16 21:55:50 +0530

    Update .gitignore and index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import dotenv from 'dotenv';
 import { dbConnect } from './db';
 
 dotenv.config();
-const port = process.env.PORT;
+const port = process.env.PORT ?? 5000;
 
 const app = express();
 
